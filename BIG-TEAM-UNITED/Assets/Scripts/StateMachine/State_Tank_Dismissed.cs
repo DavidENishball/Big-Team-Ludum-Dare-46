c732550a621ad8c438cfec4c883ec69aff04d9a5
DESKTOP-6QRDQEC\DavidN <DavidENishball@gmail.com>
2020-04-18 12:02:21 -0400

    Stubbed in some classes for a state machine.  May or may not use them

---
+++ BIG-TEAM-UNITED/Assets/Scripts/StateMachine/State_Tank_Dismissed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class State_Tank_Dismissed : IState
+{
+    LifeformTank owner;
+
+    public State_Tank_Dismissed(LifeformTank owner) { this.owner = owner; }
+
+    public void Enter()
+    {
+        Debug.Log("entering State_Tank_Dismissed");
+        owner.DismissTank();
+    }
+
+    public void Execute()
+    {
+        Debug.Log("updating State_Tank_Dismissed state");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("exiting State_Tank_Dismissed state");
+    }
+}

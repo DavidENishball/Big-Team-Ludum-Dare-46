445501092c4a757d4bf832db1455fb99e33c9641
goet <goetsewer@gmail.com>
2020-04-18 18:47:48 +0200

    Added an interface for displaying texts on a terminal, also imported TMP

---
+++ BIG-TEAM-UNITED/Assets/BIG-TEAM/goet/Scripts/Terminal.cs
@@ -0,0 +1,33 @@
+﻿using deVoid.Utils;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Terminal : MonoBehaviour
+{
+    public TextMeshProUGUI tmpro;
+    public float secondsBetweenCharacters = 1f;
+
+    public void Display(string message)
+    {
+        tmpro.text = message;
+        StartCoroutine(WriteByLines());
+    }
+
+    private IEnumerator WriteByLines()
+    {
+        var totalLines = tmpro.text.Split('\n').Length;
+        var revealedLines = 0;
+
+        while (revealedLines < totalLines)
+        {
+            tmpro.maxVisibleLines = revealedLines;
+
+            revealedLines++;
+
+            yield return new WaitForSeconds(secondsBetweenCharacters);
+        }
+
+        tmpro.maxVisibleLines = totalLines;
+    }
+}

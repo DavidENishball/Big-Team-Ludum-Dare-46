6ed2792ce173729c7add397a3ef337e77f196f1f
goet <goetsewer@gmail.com>
2020-04-18 21:38:19 +0200

    Added a small delay to screen clear

--- BIG-TEAM-UNITED/Assets/BIG-TEAM/goet/Scripts/Terminal.cs
+++ BIG-TEAM-UNITED/Assets/BIG-TEAM/goet/Scripts/Terminal.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// The main controller for displaying strings on the computer.
+/// </summary>
 public class Terminal : MonoBehaviour
 {
     public float secondsBetweenLines = 1f;
+    public float secondsClearTime = 0.5f;
 
     private TextMeshProUGUI tmpro;
+    private Coroutine currentAction;
 
     private void Awake()
     {
@@ -15,14 +20,26 @@ public class Terminal : MonoBehaviour
         Signals.Get<DisplayTerminalMessageSignal>().AddListener(Display);
     }
 
+    /// <summary>
+    /// You should probably call Signals.Get<DisplayTerminalMessageSignal>().Dispatch(message) instead.
+    /// </summary>
     public void Display(string message)
     {
         tmpro.text = message;
-        StartCoroutine(WriteByLines());
+
+        if (currentAction != null)
+            StopCoroutine(currentAction);
+
+        currentAction = StartCoroutine(WriteByLines());
     }
 
     private IEnumerator WriteByLines()
     {
+        // clear screen, wait a bit
+        tmpro.maxVisibleLines = 0;
+        yield return new WaitForSeconds(secondsClearTime);
+
+        // start displaying things line by line
         var totalLines = tmpro.text.Split('\n').Length;
         var revealedLines = 1;
 

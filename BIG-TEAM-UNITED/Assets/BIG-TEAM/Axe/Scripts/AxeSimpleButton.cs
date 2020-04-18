fc6757aa8bf1cc6756e1522aeaaaf99043873bd3
Luke Davenport <luke.l.davenport@gmail.com>
2020-04-18 19:58:42 +1000

    Prototype scene and Assets

---
+++ BIG-TEAM-UNITED/Assets/BIG-TEAM/Axe/Scripts/AxeSimpleButton.cs
@@ -0,0 +1,46 @@
+﻿using deVoid.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeSimpleButton : MonoBehaviour
+{
+    public Animator Controller;
+    protected bool interactable = true;
+    public int ID = 0;
+
+    void Awake()
+    {
+        Signals.Get<EnableButtonSignal>().AddListener(EnableButton);
+        Signals.Get<DisableButtonSignal>().AddListener(DisableButton);
+    }
+
+
+    public void EnableButton(int BID)
+    {
+        if(BID == ID)
+        {
+            interactable = true;
+        }
+    }
+
+    public void DisableButton(int BID)
+    {
+        if (BID == ID)
+        {
+            interactable = false;
+        }
+    }
+
+
+    private void OnMouseDown()
+    {
+        if(interactable)
+        {
+            Controller.SetTrigger("PlayClick");
+            Signals.Get<ButtonPressedSignal>().Dispatch(ID);
+        }
+    }
+
+}
---
+++ BIG-TEAM-UNITED/Assets/BIG-TEAM/Axe/Scripts/SignalDefs.cs
@@ -0,0 +1,32 @@
+﻿
+using deVoid.Utils;
+
+/// <summary>
+/// Input Events
+/// </summary>
+//button press by ID
+public class ButtonPressedSignal : ASignal<int> { }
+//button press by Id and held time
+public class ButtonHeldSignal : ASignal<int, float> { }
+public class ButtonReleasedSignal : ASignal<int, float> { }
+
+/// <summary>
+/// Game Events
+/// </summary>
+public class StartGameSignal : ASignal { }
+public class EndGameSignal : ASignal { }
+public class ScoreSignal : ASignal<string, int> { }
+
+
+public class EnableButtonSignal : ASignal<int> { }
+public class DisableButtonSignal : ASignal<int> { }
+
+public class LightOffSignal : ASignal<int> { }
+public class LightOnGreenSignal : ASignal<int> { }
+public class LightOnYellowSignal : ASignal<int> { }
+public class LightOnRedSignal : ASignal<int> { }
+
+
+public class EggSpinUpSignal : ASignal<float> { }
+public class EggSpinDownSignal : ASignal<float> { }
+public class EggStopSignal : ASignal{ }

8aad3707f896295ec3bcd5836191ce3880b317df
Phil Blank <phil@redmoonworkshop.com>
2020-04-19 17:29:43 -0400

    Whackamole fixes

---
+++ BIG-TEAM-UNITED/Assets/Scripts/Puzzles/PuzzleManager_HiddenGoal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using deVoid.Utils;
+
+public class PuzzleManager_HiddenGoal : PuzzleManager_Base
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Signals.Get<PerformVerbSignal>().AddListener(ReceivedVerb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ReceivedVerb(Component source, LifeformManager.EControlVerbs Verb, int data)
+    {
+        if (source.gameObject.transform.IsChildOf(this.transform))
+        {
+
+        }
+    }
+}
